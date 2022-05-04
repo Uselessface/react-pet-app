097af866bea802f90d0dd9b621bf0ab4b74db0d8
Uselessface <Uselessface@mail.ru>
2022-05-04 14:43:33 +0400

    Canges 04.05

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Technologies from './Technologies';
+import Header from './Header';
+
 
 function App() {
   return (
-    <div className="App">
-        <div> <secondApp /> </div>
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <Header />
+        <Technologies />
     </div>
   );
 }
 
-const secondApp = () => {
-    return (
-        <div>
-            <header>
-                <img src={logo} className="App-logo" alt="logo" />
-            </header>
-        </div>
-    );
-}
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+function Header () {
+    return(
+        <div>
+            <ul>
+                <li><a href = "#s">Home</a></li>
+                <li><a href = "#s">News Fedd</a></li>
+                <li><a href = "#s">Messages</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
