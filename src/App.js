e7879a0c9400387533d76ca872997daac9e0fa48
Uselessface <Uselessface@mail.ru>
2022-05-11 16:37:20 +0400

    14 lesson pet project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-
+import React from "react";
 import './App.css';
-import Technologies from './Technologies';
-import Header from './Header';
+import AppHeader from "./components/AppHeader";
+import AppNavigation from "./components/AppNavigation";
+import Profile from "./components/Profile";
+import AppFooter from "./components/AppFooter";
 
 
 function App() {
   return (
-    <div >
-        <Header />
-        <Technologies />
+    <div className={'wrapper'}>
+        <AppHeader />
+        <AppNavigation />
+        <main className={'app_main_content'}>
+            <Profile />
+        </main>
+        <AppFooter />
     </div>
   );
 }
